7274e3ab709cb7710e8a8944f4f07f6896960dbc
vamshi <vamshipanuganti@gmail.com>
2016-05-28 15:54:35 +0530

    First

---
+++ TGLibrary/TGLibrary.h
@@ -0,0 +1,19 @@
+//
+//  TGLibrary.h
+//  TGLibrary
+//
+//  Created by Anirudh Loya on 5/28/16.
+//  Copyright © 2016 Vrinda TechApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TGLibrary.
+FOUNDATION_EXPORT double TGLibraryVersionNumber;
+
+//! Project version string for TGLibrary.
+FOUNDATION_EXPORT const unsigned char TGLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TGLibrary/PublicHeader.h>
+
+
